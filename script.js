0a3ae1ab2f2dab7dca41cf472637bee86353b1f8
DayFerreira-rgb <eudayanesilvaferreira@gmail.com>
2025-11-12 16:43:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+///A lista ARRAY - Vetores
+
+var nomes = ["Argentina", "França", "Estados Unidos", "Itália", "México","Brasil", "Holanda", "Alemanha", "Filipinas", "Chipre", "Croácia"]
+
+var escolhaPaís = parseInt(prompt("Digite um número de 0 a 10"))
+
+
+console.log(nomes[escolhaPaís])
+document.write( "Nome:" + nomes [escolhaPaís])
+
